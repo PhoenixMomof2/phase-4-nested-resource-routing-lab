013cdd51c5445ee2821baea17fb9bdedc66cbb0f
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-01-25 17:09:16 -0500

    assignment complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-
+  
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    user = User.find(params[:id])
+    if user
+      render json: user, include: :items
+    else
+      render json: {error: "User not found"}, status: 404
+    end
   end
 
 end
